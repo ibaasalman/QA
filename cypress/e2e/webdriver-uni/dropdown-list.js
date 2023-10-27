5836399284c074bf1d50bb8ab3d7b593fcf2f1c7
ibaa <ibaasalman0@gmail.com>
2023-10-27 15:59:59 +0300

    add dropdown lists

---
+++ cypress/e2e/webdriver-uni/dropdown-list.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("Interact with dropdown lists via webdriveruni", () => {
+    it("Select specific values via select dropdown lists", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
+
+        cy.get('#dropdowm-menu-1').select('c#')
+        cy.get('#dropdowm-menu-2').select('testng').should('have.value', 'testng')
+        cy.get('#dropdowm-menu-3').select('JQuery').contains('JQuery')
+    });
+})
