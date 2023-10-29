bfcc92f4513556ee36fa280a9c9f42e3d85a92f6
ibaa <ibaasalman0@gmail.com>
2023-10-29 16:51:10 +0200

    Useing fixtures

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,6 +1,20 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us page", () => {
+  before(function () {
+
+    cy.fixture("example").then((d)=>{
+      //this.data = d;
+      globalThis.data = d;
+    })
+    //cy.fixture("example").as("data")
+    //then in each test get data from ("data") and handle it in then => {}
+    // cy.get("@data").then((user) => {
+    //   cy.get("#ContactUsFrm_first_name").type(user.first_name);
+    //   cy.get("#ContactUsFrm_email").type(user.email);
+    // });
+  });
+
   beforeEach(function () {
     //cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
 
@@ -9,6 +23,7 @@ describe("Test Contact Us page", () => {
     cy.visit("https://www.webdriveruniversity.com/");
     cy.get("#contact-us").invoke("removeAttr", "target").click();
   });
+
   it("cheack page title and charset", () => {
     cy.document().should("have.property", "charset").and("eq", "UTF-8");
     cy.title().should("include", "WebDriver | Contact Us");
@@ -16,10 +31,10 @@ describe("Test Contact Us page", () => {
   });
 
   it("fill all required inputs and submit successfully", () => {
-    cy.get('[placeholder="First Name"]').type("ibaa");
-    cy.get('[placeholder="Last Name"]').type("salman");
-    cy.get('[placeholder="Email Address"]').type("example@gmail.com");
-    cy.get('[placeholder="Comments"]').type("Hello World :) ");
+    cy.get('[placeholder="First Name"]').type(data.firstname);
+    cy.get('[placeholder="Last Name"]').type(data.lastname);
+    cy.get('[placeholder="Email Address"]').type(data.email);
+    cy.get('[placeholder="Comments"]').type(data.body);
     cy.get('[Value="SUBMIT"]').click();
     cy.get("#contact_reply > h1").should(
       "have.text",
