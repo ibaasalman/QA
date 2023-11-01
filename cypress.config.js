6831eb550477c7debea83cc1af03328a6d486707
ibaa <ibaasalman0@gmail.com>
2023-11-01 11:15:22 +0200

    working with reports

--- cypress.config.js
+++ cypress.config.js
@@ -5,12 +5,18 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern : "cypress/e2e/**/*.{js,ts,jsx,tsx,feature}",
-    chromeWebSecurity:false,
-    excludeSpecPattern : ["cypress/e2e/2-advanced-examples/*.js","cypress/e2e/1-getting-started/*.js"],
-    screenshotOnRunFailure:true,
-    trashAssetsBeforeRuns:true
+    specPattern: "cypress/e2e/**/*.{js,ts,jsx,tsx,feature}",
+    chromeWebSecurity: false,
+    excludeSpecPattern: [
+      "cypress/e2e/2-advanced-examples/*.js",
+      "cypress/e2e/1-getting-started/*.js",
+    ],
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
     //the last 2 lines are for taking screenshots and delet it before next run
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json",
+    },
   },
- 
 });
